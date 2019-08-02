82474e0df068d32d1c5fcf158aba01b31632fe22
joanna waszkiewicz <joanna.waszkiewicz@pagepro.pl>
2019-08-02 14:26:10 +0200

    showMore, class component

--- src/features/blog/ArticleList/index.js
+++ src/features/blog/ArticleList/index.js
@@ -1,60 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { fakeData } from './mocks'
 import ArticleCard from '../ArticleCard'
 import {ArticleWrapper } from './styles'
 
-// class ArticleList extends React.Component {
-//   constructor(props) {
-//     super(props)
+class ArticleList extends React.Component {
+  constructor(props) {
+    super(props)
 
-//     this.state = {
-//       data: [...fakeData],
-//       visible: 2
-//     }
+    this.state = {
+      loading: false,
+      data: [],
+      total: 0
+    }
 
-//     this.showMore = this.showMore.bind(this)
-//   }
-
-//   showMore() {
-//     this.setState((prev) => {
-//       return {visible: prev.visible + 2};
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <ArticleWrapper>
-//         {this.state.data
-//           .slice(0, this.state.visible)
-//           .map(data => <ArticleCard {...data} key={data.id} />)
-//         }
-//         <button onClick={this.showMore}>
-//             Show More
-//         </button>
-//       </ArticleWrapper>
-//     )
-//   }
-// }
-
-const ArticleList2 = () => {
-  const [ showMore, setShowMore] = useState(2)
-
-  const loadMore = () => {
-    setShowMore(showMore + 2)
+    this.showMore = this.showMore.bind(this)
   }
 
-  return (
-    <ArticleWrapper>
-      {fakeData
-        .slice(0, showMore)
-        .map(data => <ArticleCard {...data} key={data.id} />)
+  showMore() {
+    this.setState((prev) => {
+      return {
+        total: prev.total + 2,
+        data: [...fakeData]
       }
-      <button onClick={loadMore}>
-          Show More
-      </button>
-    </ArticleWrapper>
-  )
+    })
+  }
+
+  componentWillMount() {
+    this.showMore()
+  }
+
+  render() {
+    return (
+      <ArticleWrapper>
+        {this.state.data
+          .slice(0, this.state.total)
+          .map(data => <ArticleCard {...data} key={data.id} />)
+        }
+        {this.state.total < this.state.data.length &&
+          <button onClick={this.showMore}>
+              Show More
+          </button>
+        }
+      </ArticleWrapper>
+    )
+  }
 }
 
-// export default ArticleList
-export default ArticleList2
+export default ArticleList
