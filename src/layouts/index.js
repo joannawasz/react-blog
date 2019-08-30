a48117604c5d0ffb10e38618730a4bdf5d3eac7c
joanna waszkiewicz <joanna.waszkiewicz@pagepro.pl>
2019-08-30 15:08:51 +0200

    toast location change

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 import './index.scss'
 import Routing from '../config/Routing'
 import 'react-toastify/dist/ReactToastify.css'
+
+toast.configure({
+  position: toast.POSITION.TOP_LEFT,
+  autoClose: 5000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+})
 
 const App = () => {
   return <Routing />
